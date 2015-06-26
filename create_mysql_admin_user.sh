f9265f05d94a1a2e7a0a5a5bbf33c1123b295728
刘红 <liuhong1.happy@163.com>
2015-06-26 22:20:22 +0800

    Update create_mysql_admin_user.sh

--- create_mysql_admin_user.sh
+++ create_mysql_admin_user.sh
@@ -10,11 +10,8 @@ while [[ RET -ne 0 ]]; do
     RET=$?
 done
 
-PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
-_word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
-echo "=> Creating MySQL admin user with ${_word} password"
 
-mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
+mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$MYSQL_PASS'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
 
 
@@ -23,7 +20,7 @@ echo "=> Done!"
 echo "========================================================================"
 echo "You can now connect to this MySQL Server using:"
 echo ""
-echo "    mysql -uadmin -p $PASS -h <host> -P <port>"
+echo "    mysql -uadmin -p $MYSQL_PASS -h <host> -P <port>"
 echo ""
 echo "Please remember to change the above password as soon as possible!"
 echo "MySQL user 'root' has no password but only allows local connections"
